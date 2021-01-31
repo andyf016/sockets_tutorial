79c4d7faf471152830117e382475dbd3fa8c8672
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-31 02:26:00 -0500

    intital commit of sockets project

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+# A socket is an endpoint that recieves data
+# socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Tuple of IP and Port
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    # accpt all incomming connections
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established")
+    # send information to client socket
+    clientsocket.send()
