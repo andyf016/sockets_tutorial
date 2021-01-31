50438bc43f608cd17cd237dfe3d15d077e481c52
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-31 03:14:36 -0500

    set header

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 
+
+
+
+
 # A socket is an endpoint that recieves data
 # socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,5 +16,11 @@ while True:
     # accpt all incomming connections
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established")
+
+    msg = "Welcome to the Server"
+    # Fixed length header
+    msg = print(f'{len(msg): < 10}' + msg)
+
     # send information to client socket
     clientsocket.send(bytes("Welcome to the server", "utf-8"))
+    clientsocket.close()
