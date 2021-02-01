a0a55749c3f7db2a14806a1c2afdb9292e9c3599
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-01 00:16:57 -0500

    finish pickle for client

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ s.connect((socket.gethostname(), 1235))
 
 while True:
 
-    full_msg = ''
+    full_msg = b''
     new_msg = True
     while True:
         msg = s.recv(16)
@@ -17,12 +17,15 @@ while True:
             print(f"new message length: {msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE])
             new_msg = False
-        full_msg += msg.decode("utf-8")
-
+        full_msg += msg
         if len(full_msg) - HEADERSIZE == msglen:
             print('full mesage recieved')
             print(full_msg[HEADERSIZE:])
+
+            d = pickle.loads(full_msg[HEADERSIZE:])
+            print(d)
+
             new_msg = True
-            full_msg = ''
+            full_msg = b''
 
     print(full_msg)
